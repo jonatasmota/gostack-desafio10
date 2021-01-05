ef3c0f46efeb6d4599c54f64aea1deac76f65f46
jonatasmota <jonatascmota@gmail.com>
2021-01-05 16:08:07 -0200

    Desafio 10 Concluido

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import api from '../../services/api';
+
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
@@ -27,11 +29,23 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    try {
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable,
+      });
+
+      setIsAvailable(!isAvailable);
+    } catch (err) {
+      console.log(err);
+    }
+
+
+
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+   handleEditFood(food);
   }
 
   return (
